0c92aebb84e0c544bbd09f74598f3615cd4b6885
Aaryan Neupane <sonofnaleen@gmail.com>
2022-12-08 21:56:30 +0545

    dummy data added

--- lib/providers/driver_location.dart
+++ lib/providers/driver_location.dart
@@ -30,6 +30,342 @@ class DriverLocation with ChangeNotifier {
         'longitude': '85.3234030602186',
       },
     },
+    {
+      'id': 1,
+      'coordinates': {
+        'latitude': '27.73066283502807',
+        'longitude': ' 85.34758484239046',
+      },
+    },
+    {
+      'id': 2,
+      'coordinates': {
+        'latitude': '27.730195645064825',
+        'longitude': '85.35621853819345',
+      },
+    },
+    {
+      'id': 3,
+      'coordinates': {
+        'latitude': '27.67920472602175',
+        'longitude': '85.32652301220861',
+      },
+    },
+    {
+      'id': 4,
+      'coordinates': {
+        'latitude': '27.680974791341768',
+        'longitude': '85.3234030602186',
+      },
+    },
+    {
+      'id': 1,
+      'coordinates': {
+        'latitude': '27.73066283502807',
+        'longitude': ' 85.34758484239046',
+      },
+    },
+    {
+      'id': 2,
+      'coordinates': {
+        'latitude': '27.730195645064825',
+        'longitude': '85.35621853819345',
+      },
+    },
+    {
+      'id': 3,
+      'coordinates': {
+        'latitude': '27.67920472602175',
+        'longitude': '85.32652301220861',
+      },
+    },
+    {
+      'id': 4,
+      'coordinates': {
+        'latitude': '27.680974791341768',
+        'longitude': '85.3234030602186',
+      },
+    },
+    {
+      'id': 1,
+      'coordinates': {
+        'latitude': '27.73066283502807',
+        'longitude': ' 85.34758484239046',
+      },
+    },
+    {
+      'id': 2,
+      'coordinates': {
+        'latitude': '27.730195645064825',
+        'longitude': '85.35621853819345',
+      },
+    },
+    {
+      'id': 3,
+      'coordinates': {
+        'latitude': '27.67920472602175',
+        'longitude': '85.32652301220861',
+      },
+    },
+    {
+      'id': 4,
+      'coordinates': {
+        'latitude': '27.680974791341768',
+        'longitude': '85.3234030602186',
+      },
+    },
+    {
+      'id': 1,
+      'coordinates': {
+        'latitude': '27.73066283502807',
+        'longitude': ' 85.34758484239046',
+      },
+    },
+    {
+      'id': 2,
+      'coordinates': {
+        'latitude': '27.730195645064825',
+        'longitude': '85.35621853819345',
+      },
+    },
+    {
+      'id': 3,
+      'coordinates': {
+        'latitude': '27.67920472602175',
+        'longitude': '85.32652301220861',
+      },
+    },
+    {
+      'id': 4,
+      'coordinates': {
+        'latitude': '27.680974791341768',
+        'longitude': '85.3234030602186',
+      },
+    },
+    {
+      'id': 1,
+      'coordinates': {
+        'latitude': '27.73066283502807',
+        'longitude': ' 85.34758484239046',
+      },
+    },
+    {
+      'id': 2,
+      'coordinates': {
+        'latitude': '27.730195645064825',
+        'longitude': '85.35621853819345',
+      },
+    },
+    {
+      'id': 3,
+      'coordinates': {
+        'latitude': '27.67920472602175',
+        'longitude': '85.32652301220861',
+      },
+    },
+    {
+      'id': 4,
+      'coordinates': {
+        'latitude': '27.680974791341768',
+        'longitude': '85.3234030602186',
+      },
+    },
+    {
+      'id': 1,
+      'coordinates': {
+        'latitude': '27.73066283502807',
+        'longitude': ' 85.34758484239046',
+      },
+    },
+    {
+      'id': 2,
+      'coordinates': {
+        'latitude': '27.730195645064825',
+        'longitude': '85.35621853819345',
+      },
+    },
+    {
+      'id': 3,
+      'coordinates': {
+        'latitude': '27.67920472602175',
+        'longitude': '85.32652301220861',
+      },
+    },
+    {
+      'id': 4,
+      'coordinates': {
+        'latitude': '27.680974791341768',
+        'longitude': '85.3234030602186',
+      },
+    },
+    {
+      'id': 1,
+      'coordinates': {
+        'latitude': '27.73066283502807',
+        'longitude': ' 85.34758484239046',
+      },
+    },
+    {
+      'id': 2,
+      'coordinates': {
+        'latitude': '27.730195645064825',
+        'longitude': '85.35621853819345',
+      },
+    },
+    {
+      'id': 3,
+      'coordinates': {
+        'latitude': '27.67920472602175',
+        'longitude': '85.32652301220861',
+      },
+    },
+    {
+      'id': 4,
+      'coordinates': {
+        'latitude': '27.680974791341768',
+        'longitude': '85.3234030602186',
+      },
+    },
+    {
+      'id': 1,
+      'coordinates': {
+        'latitude': '27.73066283502807',
+        'longitude': ' 85.34758484239046',
+      },
+    },
+    {
+      'id': 2,
+      'coordinates': {
+        'latitude': '27.730195645064825',
+        'longitude': '85.35621853819345',
+      },
+    },
+    {
+      'id': 3,
+      'coordinates': {
+        'latitude': '27.67920472602175',
+        'longitude': '85.32652301220861',
+      },
+    },
+    {
+      'id': 4,
+      'coordinates': {
+        'latitude': '27.680974791341768',
+        'longitude': '85.3234030602186',
+      },
+    },
+    {
+      'id': 1,
+      'coordinates': {
+        'latitude': '27.73066283502807',
+        'longitude': ' 85.34758484239046',
+      },
+    },
+    {
+      'id': 2,
+      'coordinates': {
+        'latitude': '27.730195645064825',
+        'longitude': '85.35621853819345',
+      },
+    },
+    {
+      'id': 3,
+      'coordinates': {
+        'latitude': '27.67920472602175',
+        'longitude': '85.32652301220861',
+      },
+    },
+    {
+      'id': 4,
+      'coordinates': {
+        'latitude': '27.680974791341768',
+        'longitude': '85.3234030602186',
+      },
+    },
+    {
+      'id': 1,
+      'coordinates': {
+        'latitude': '27.73066283502807',
+        'longitude': ' 85.34758484239046',
+      },
+    },
+    {
+      'id': 2,
+      'coordinates': {
+        'latitude': '27.730195645064825',
+        'longitude': '85.35621853819345',
+      },
+    },
+    {
+      'id': 3,
+      'coordinates': {
+        'latitude': '27.67920472602175',
+        'longitude': '85.32652301220861',
+      },
+    },
+    {
+      'id': 4,
+      'coordinates': {
+        'latitude': '27.680974791341768',
+        'longitude': '85.3234030602186',
+      },
+    },
+    {
+      'id': 1,
+      'coordinates': {
+        'latitude': '27.73066283502807',
+        'longitude': ' 85.34758484239046',
+      },
+    },
+    {
+      'id': 2,
+      'coordinates': {
+        'latitude': '27.730195645064825',
+        'longitude': '85.35621853819345',
+      },
+    },
+    {
+      'id': 3,
+      'coordinates': {
+        'latitude': '27.67920472602175',
+        'longitude': '85.32652301220861',
+      },
+    },
+    {
+      'id': 4,
+      'coordinates': {
+        'latitude': '27.680974791341768',
+        'longitude': '85.3234030602186',
+      },
+    },
+    {
+      'id': 1,
+      'coordinates': {
+        'latitude': '27.73066283502807',
+        'longitude': ' 85.34758484239046',
+      },
+    },
+    {
+      'id': 2,
+      'coordinates': {
+        'latitude': '27.730195645064825',
+        'longitude': '85.35621853819345',
+      },
+    },
+    {
+      'id': 3,
+      'coordinates': {
+        'latitude': '27.67920472602175',
+        'longitude': '85.32652301220861',
+      },
+    },
+    {
+      'id': 4,
+      'coordinates': {
+        'latitude': '27.680974791341768',
+        'longitude': '85.3234030602186',
+      },
+    },
   ];
 
   List get locations {
